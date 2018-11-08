4e3e4246744ecb63494dd6097ec8a648059d4afd
CarmenBarreira <csilbarreira@gmail.com>
2018-11-08 17:55:55 -0300

    se agrega excepciones

--- DataAccess/AgendaDataAccess.cs
+++ DataAccess/AgendaDataAccess.cs
@@ -4,9 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Linq;
+using System.Data.Common;
+using System.Data.Entity.Core;
 
 namespace DataAccess
 {
@@ -14,67 +13,141 @@ namespace DataAccess
     {
         public void Add(Agenda entity)
         {
-            using (FriendContext context = new FriendContext())
+            try
             {
-                context.Agendas.Add(entity);
-                context.SaveChanges();
+                using (FriendContext context = new FriendContext())
+                {
+                    context.Agendas.Add(entity);
+                    context.SaveChanges();
+                }
+            }
+            catch (DbException e)
+            {
+                throw new AccesoDatosBDException();
+
+            }
+            catch (UpdateException exception)
+            {
+                throw new AccesoDatosBDException();
             }
         }
 
         public void Delete(Agenda entity)
         {
-            using (FriendContext context = new FriendContext())
+            try
             {
-                var customer = context.Agendas.Single(o => o.Id == entity.Id);
-                context.Agendas.Remove(customer);
-                context.SaveChanges();
+                using (FriendContext context = new FriendContext())
+                {
+                    var customer = context.Agendas.Single(o => o.Id == entity.Id);
+                    context.Agendas.Remove(customer);
+                    context.SaveChanges();
+                }
+            }
+            catch (DbException e)
+            {
+                throw new AccesoDatosBDException();
+
+            }
+            catch (UpdateException exception)
+            {
+                throw new AccesoDatosBDException();
             }
         }
 
         public Agenda Get(Guid id)
         {
-            using (FriendContext context = new FriendContext())
+            try
             {
-                Agenda agenda = context.Agendas.FirstOrDefault(a => a.Id == id);
-                context.Entry(agenda).Reference(a => a.Owner).Load();
-                context.Entry(agenda).Collection(a => a.Contacts).Load(); 
-                return agenda;
+                using (FriendContext context = new FriendContext())
+                {
+                    Agenda agenda = context.Agendas.FirstOrDefault(a => a.Id == id);
+                    context.Entry(agenda).Reference(a => a.Owner).Load();
+                    context.Entry(agenda).Collection(a => a.Contacts).Load();
+                    return agenda;
+                }
+            }
+            catch (DbException e)
+            {
+                throw new AccesoDatosBDException();
+
+            }
+            catch (UpdateException exception)
+            {
+                throw new AccesoDatosBDException();
             }
         }
 
+
         public Agenda Get(string name)
         {
-            using (FriendContext context = new FriendContext())
+            try
             {
-                Agenda agenda = context.Agendas.FirstOrDefault(a => a.Name == name);
-                context.Entry(agenda).Reference(a => a.Owner).Load();
-                context.Entry(agenda).Collection(a => a.Contacts).Load();
-                return agenda;
+                using (FriendContext context = new FriendContext())
+                {
+                    Agenda agenda = context.Agendas.FirstOrDefault(a => a.Name == name);
+                    context.Entry(agenda).Reference(a => a.Owner).Load();
+                    context.Entry(agenda).Collection(a => a.Contacts).Load();
+                    return agenda;
+                }
+            }
+            catch (DbException e)
+            {
+                throw new AccesoDatosBDException();
+
+            }
+            catch (UpdateException exception)
+            {
+                throw new AccesoDatosBDException();
             }
         }
 
         public ICollection<Agenda> GetAll()
         {
-            using (FriendContext context = new FriendContext())
+            try
             {
-                return context.Agendas.ToList();
+                using (FriendContext context = new FriendContext())
+                {
+                    return context.Agendas.ToList();
+                }
+            }
+            catch (DbException e)
+            {
+                throw new AccesoDatosBDException();
+
+            }
+            catch (UpdateException exception)
+            {
+                throw new AccesoDatosBDException();
             }
         }
 
         public void Modify(Agenda entity)
         {
-            using (FriendContext context = new FriendContext())
+            try
             {
-                context.Entry(entity).State = EntityState.Modified;
-                context.Entry(entity.Owner).State = EntityState.Modified;
-
-                foreach (var contact in entity.Contacts)
+                using (FriendContext context = new FriendContext())
                 {
-                    context.Entry(contact).State = contact.Id.Equals(Guid.Empty) ? EntityState.Added : EntityState.Unchanged;
-                }
+                    context.Entry(entity).State = EntityState.Modified;
+                    context.Entry(entity.Owner).State = EntityState.Modified;
 
-                context.SaveChanges();
+                    foreach (var contact in entity.Contacts)
+                    {
+                        context.Entry(contact).State = contact.Id.Equals(Guid.Empty) ? EntityState.Added : EntityState.Unchanged;
+                    }
+
+                    context.SaveChanges();
+                }
             }
+            catch (DbException e)
+            {
+                throw new AccesoDatosBDException();
+
+            }
+            catch (UpdateException exception)
+            {
+                throw new AccesoDatosBDException();
+            }
+
         }
     }
 }
---
+++ Interfaces/AccesoDatosBDException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public class AccesoDatosBDException : Exception
+    {
+        public const string MESSAGE = "Error en el acceso a la BD. Comuniquese con Administrador";
+        public AccesoDatosBDException()
+        {
+        }
+    }
+}
